23d0285b6c3f991491907b7a9dc508fd40b07419
codeschre <victormwendwa2002@gmail.com>
2021-11-30 13:56:40 +0000

    hh

--- js/script.js
+++ js/script.js
@@ -15,3 +15,13 @@ document.querySelectorAll('.about .video-container .controls .control-btn').forE
     }
 })
 
+if ($(".blogBox:hidden").length != 0) {
+    $("#loadMore").show();
+  }   
+  $("#loadMore").on('click', function (e) {
+    e.preventDefault();
+    $(".moreBox:hidden").slice(0, 6).slideDown();
+    if ($(".moreBox:hidden").length == 0) {
+      $("#loadMore").fadeOut('slow');
+    }
+  });
